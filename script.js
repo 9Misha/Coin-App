d63600790e7e25f33661a059627244f4f3a67de8
mykhailo_yeroshevych <yeroshevich.misha@gmail.com>
2024-03-06 16:39:57 +0200

    Додав посилання, але чомусь воно не стає в потрібну ячейку

--- script.js
+++ script.js
@@ -24,16 +24,61 @@ async function displayData() {
       
       for (const el of coinData) {
          const tableBody = document.getElementById('table-body');
-         const row = document.createElement("tr")
+         // Creating tr section
+         const row = document.createElement("tr");
 
-         const imageCell = document.createElement("td")
-         const nameCell = document.createElement("td")
-         const currentPriceCell = document.createElement("td")
+         // Creating cells in the table
+         const imageCell = document.createElement('img');
+         const nameCell = document.createElement('a');
+         const nameCellText = document.createTextNode(el.name);
+         const currentPriceCell = document.createElement('td');
+         const high24Cell = document.createElement('td');
+         const pers24hoursCell = document.createElement('td');
+         const pers7dayCell = document.createElement('td');
+         const pers30dayCell = document.createElement('td');
+         const pers200dayCell = document.createElement('td');
+         const pers1yearCell = document.createElement('td');
 
-         nameCell.textContent = el.name;
 
-         row.appendChild(nameCell)
-         tableBody.appendChild(row)
+         // Filling the content in the table cells
+         imageCell.setAttribute('src', el.image);
+         imageCell.style.height = '50px';
+         imageCell.style.width = '45px';
+
+
+         // Creating a link 
+         nameCell.appendChild(nameCellText);
+         nameCell.title = `${el.name}`;
+         nameCell.href = 'https://stackoverflow.com/questions/4772774/how-do-i-create-a-link-using-javascript'
+
+         
+         currentPriceCell.textContent = (el.current_price).toFixed(5);
+         high24Cell.textContent = Number(el.high_24h).toFixed(6);
+         pers24hoursCell.textContent = Number(el.price_change_percentage_24h_in_currency).toFixed(6);
+         pers7dayCell.textContent = Number(el.price_change_percentage_7d_in_currency).toFixed(6);
+         pers30dayCell.textContent = Number(el.price_change_percentage_30d_in_currency).toFixed(6);
+         pers200dayCell.textContent = Number(el.price_change_percentage_200d_in_currency).toFixed(6);
+         pers1yearCell.textContent = Number(el.price_change_percentage_1y_in_currency).toFixed(6);
+
+
+
+
+         // Adding another one row in the end of the table
+         row.appendChild(imageCell);
+         row.appendChild(nameCell); 
+         row.appendChild(currentPriceCell);
+         row.appendChild(high24Cell);
+         row.appendChild(pers24hoursCell);
+         row.appendChild(pers7dayCell);
+         row.appendChild(pers30dayCell);
+         row.appendChild(pers200dayCell);
+         row.appendChild(pers1yearCell); 
+
+        
+         /*appendChild - закидує елемент в кінець списку, далі йде по ієрархії. Тобто nameCell буде знаходитись в батьківському тезі <tr></tr>*/
+         tableBody.appendChild(row);
+
+         console.log(el);
          
 
          // let table = ''
